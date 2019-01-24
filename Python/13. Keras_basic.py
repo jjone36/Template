f3dca34d177e5bab48534eecac71c122e2e6051d
jjone <jiwon.j306@gmail.com>
2019-01-24 13:03:32 +0900

    keras update & colab setup final

--- Python/13. Keras_basic.py
+++ Python/13. Keras_basic.py
@@ -71,7 +71,6 @@ model = Model(inputs = [in_tensor_1, in_tensor_2], outputs = out_tensor)
 import keras
 from keras.models import Sequential
 from Keras.layers import Dense
-from keras.callbacks import EarlyStopping
 
 # initialize the model
 model = Sequential()
@@ -84,7 +83,9 @@ model.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))  # so
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 # -> 'mean_squared_error' / 'binary_crossentropy'
 
-early_stopper = EarlyStopping(patience = 2)
+# early_stopper
+from keras.callbacks import EarlyStopping
+early_stopper = EarlyStopping(patience = 5)
 
 model.fit(X_train, y_train, batch_size = 10, nb_epoch = 100, callbacks = [early_stopper])   # validation_split
 y_pred = model.predict(X_test)
@@ -95,11 +96,7 @@ plt.plot(history['loss'])
 plt.plot(history['val_loss'])
 plt.show()
 
-# Monitoring validation loss
-from keras.callbacks import ModelCheckpoint
-checkpoint = ModelCheckpoint('weights.hdf5', monitor = 'val_loss', save_best_only = True)
 
-callbacks_list = [checkpoint]
 # Fit the model using the checkpoint
 model.fit(X_train, y_train, epochs = 10, validation_split = .2, callbacks = callbacks_list)
 
@@ -128,24 +125,39 @@ for lr in lr_to_test:
     model.compile(optimizer = my_optimizer, loss = 'categorical_crossentropy')
     model.fit(predictors, target)
 
+
+##############################################################
 ############# Convolutional Neural Networks #############
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+from keras.layers import Conv2D, MaxPool2D, Flatten, Dense, BatchNormalization
+
+# importing datasets
+categories = []
+for filename in filenames:
+    category = filename.split('.')[0]
+    if category == 'dog':
+        categories.append(1)
+    else:
+        categories.append(0)
+
+im_height
+im_weight
+im_size = (im_height, im_width)
+batch_size
 
 # initialize the model
 model = Sequential()
-
 # convolutional layer
-model.add(Convolution2D(32, 3, 3), input_shape = (64, 64, 3), activation = 'relu')    # dilation_rate
-model.add(Conv2D(10, kernel_size = 3, inpurt_shape = (64, 64, 3), activation = 'relu', padding = 'same', strides = 5))
+model.add(Conv2D(32, kernel_size = 3, inpurt_shape = (im_width, im_height, 3), activation = 'relu', padding = 'same', strides = 5))
           # Convolution2D(10, 3, 3)
+# Normalization
+model.add(BatchNormalization())
 
 # adding new convolutional layer
 # model.add(Convolution2D(64, 3, 3), activation = 'relu')
 
 # pooling layer
 model.add(MaxPool2D(2))
-
 # into one big array
 model.add(Flatten())
 
@@ -154,11 +166,67 @@ model.add(Dense(output_dim = 128, activation = 'relu'))
 model.add(Dense(output_dim = 5, activation = 'softmax'))
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
+model.summary()
+
 # Fit the model
 model.fit(X_train, y_train, epochs = 10, batch_size = 64, validation_split = .2, verbose = True)
 model.evaluate(x = X_dev, y = y_dev)
 
-model.summary()
+############# image data preprocessing
+from keras.preprocessing.image import ImageDataGenerator
+train_gen = ImageDataGenerator(rescale=1./255,
+                               shear_range=0.2,
+                               zoom_range=0.2,
+                               horizontal_flip=True)
+train_generator = train_gen.flow_from_directory(dir,
+                                                target_size=(64, 64),
+                                                batch_size=32,
+                                                models_mode='binary')
+
+val_gen = ImageDataGenerator(rescale = 1./255)
+val_generator = val_gen.flow_from_dataframe(val_df, dir,
+                                            x_col='filename',
+                                            target_size= im_size,
+                                            class_mode='binary',
+                                            batch_size= batch_size)
+
+history = model.fit_generator(train_generator,
+                    epochs = 50,
+                    validation_data = val_generator,
+                    validation_steps = val_df.shape[0] // batch_size,
+                    steps_per_epoch= train_df.shape[0] // batch_size,
+                    callbacks = callbacks)
+
+# model_saving
+model.save('model_file.h5')
+
+# prediction
+# Augmentation with test set
+test_gen = ImageDataGenerator(rescale=1./255)
+test_generator = test_gen.flow_from_dataframe(test_df, dir,
+                                              x_col='filename',
+                                              y_col='category',
+                                              target_size = im_size,
+                                              batch_size = batch_size)
+predict = model.predict_generator(test_generator, steps = n_samples // batch_size))
+test_df['prob'] = predict
+test_df['category'] = np.where(test_df['prob'] > .5, 1,0)
+
+############# callbacks
+# Learning rate reduction
+from keras.callbacks import ReduceLROnPlateau
+learning_rate_reducer = ReduceLROnPlateau(monitor='val_acc',
+                                          patience=2,
+                                          verbose=1,
+                                          factor=0.5,
+                                          min_lr=0.00001)
+
+# Monitoring validation loss
+from keras.callbacks import ModelCheckpoint
+checker = ModelCheckpoint('weights.hdf5', monitor = 'val_loss', save_best_only = True)
+
+callbacks = [early_stopper, learning_rate_reducer, checker]
+
 
 ############# Get the weights
 conv_1 = model.layers[0]
@@ -175,30 +243,3 @@ plt.show()
 out = convolution(X_test[1, :, :, 0], kernels_1)
 plt.imshow(out)
 plt.show()
-
-############# image data preprocessing
-'https://keras.io/preprocessing/image/'
-from keras.preprocessing.image import ImageDataGenerator
-
-X_traingen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255)
-train_generator = X_traingen.flow_from_directory('data/train',
-                                                    target_size=(64, 64),
-                                                    batch_size=32,
-                                                    models_mode='binary')
-validation_generator = test_datagen.flow_frgooom_directory('data/validation',
-                                                        target_size=(64, 64),
-                                                        batch_size=32,
-                                                        models_mode='binary')
-model.fit_generator(train_generator,
-                   steps_per_epoch=2000,
-                   epochs=50,
-                   validation_data = validation_generator,
-                   validation_steps=800)
-
-
-#################################################
